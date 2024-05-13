50d1923ae9aabfb714652ab091a83f4e5e2d5152
Detlef Groth <dgroth@uni-potsdam.de>
2024-05-13 10:42:44 +0200

    paul package now version 0.2.0

--- paul/pkgIndex.tcl
+++ paul/pkgIndex.tcl
@@ -1,5 +1,5 @@
 
 if {![package vsatisfies [package provide Tcl] 8.6]} {return}
 
-package ifneeded paul 0.1 [list source [file join $dir paul.tcl]]
+package ifneeded paul 0.2.0 [list source [file join $dir paul.tcl]]
 
