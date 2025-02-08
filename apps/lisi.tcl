9ccda92301b6fbea0542cd004180ce96e1ee9ec4
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2025-02-08 09:07:14 +0100

    showcase lisi - graphics made easy continued

--- apps/lisi.tcl
+++ apps/lisi.tcl
@@ -5,7 +5,7 @@
 #  Author        : $Author$
 #  Created By    : MicroEmacs User
 #  Created       : 2025-02-06 06:12:50
-#  Last Modified : <250207.0622>
+#  Last Modified : <250208.0906>
 #
 #  Description	 :
 #
@@ -36,12 +36,43 @@ namespace eval ::lisi {
     set types {
         {{Music ABC Files}    {.abc} } 
         {{GraphViz Dot Files} {.dot} }                
+        {{Pikchr Files}       {.pik} }                
         {{Plantuml Files}     {.pml} }        
         {{GO SGF Files}       {.sgf} }                
         {{Text Files}         {.txt} }
         {{All Files}          *      }
     }
-
+    ## TODO check Windows MSYS /  MacOS
+    set configfolder [file join $::env(HOME) .config lisi]
+    if {![file isdir $configfolder]} {
+        file mkdir $configfolder
+    }
+    array set defaults [list \
+                        sgf {sgf-render sgf-render %s --format png --outfile %s --width 600} \
+                        pml {plantuml -tpng %i} \
+                        dot {dot -Tpng %i -o%o} \
+                        abc {abcm2ps ...} \
+                        pik {fossil pikchr %i  %b.svg&cairosvg -f png -o %o -%b.svg}]
+    foreach key [array names defaults] {
+        if {![file exists [file join $configfolder $key.opt]]} {
+            set out [open [file join $configfolder $key.opt] w 0600]
+            puts $out $defaults($key)
+            close $out
+        }
+    }
+    foreach file [glob -nocomplain $configfolder/*] {
+        if  {[file extension $file] eq ".opt"} {
+            if [catch {open $file r} infh] {
+                return error -code "Cannot open $file: $infh"
+            } else {
+                set cmd [string trim [read $infh]]
+                close $infh
+                set ext [file rootname [file tail $file]]
+                puts "$file - ext: $ext - cmd: '$cmd'"
+                set defaults($ext) $cmd
+            }
+        }
+    }
 }
 
 proc ::lisi::usage {app} {
@@ -85,14 +116,20 @@ proc ::lisi::gui {args} {
 }
 
 proc ::lisi::main {argv} {
+    variable defaults
     set filename [lindex $argv 0]
     if {![file exists $filename]} {
         puts "Error: File '$filename' does not exists!"
         usage $::argv0
     }
     set optfile [file rootname $filename].opt
+    set ext [substr [file extension $filename] 1 end]
     if {[llength $argv] == 1 && ![file exists $optfile]} {
-        set cmd "Enter your command line here!"
+        if {[info exists defaults($ext)]} {
+            set cmd $defaults($ext)
+        } else {
+            set cmd "Enter your command line here!"
+        }
     } elseif {[llength $argv] == 1} {
         if [catch {open $optfile r} infh] {
             return -code error "Cannot open $optfile: $infh"
